e42ca8c71391c0e8d17973d22b0aa6ac0a72c4e4
johnnyferns14 <johnnyferns@gmail.com>
2021-08-03 06:27:57 +0000

    functions added to delete & view categories

--- gadget/models.py
+++ gadget/models.py
@@ -9,13 +9,17 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_name')
+    name = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='category_name')
     title = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(blank=False, null=False)
-    asin = models.CharField(max_length=10, primary_key=True, null=False, blank=False)
+    asin = models.CharField(
+        max_length=10, primary_key=True, null=False, blank=False)
     price = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False)
     quantity = models.IntegerField(default=0)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- gadget/views.py
+++ gadget/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .forms import CategoryForm
+from .models import Category
+
+
+def view_categories(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'categories_view.html', context)
 
 
 def view_gadget(request):
@@ -15,3 +22,11 @@ def add_category(request):
             form.save()
     context = {'form': form}
     return render(request, 'add_category.html', context)
+
+
+def delete_category(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    if request.method == 'POST':
+        category.delete()
+    context = {'category': category, 'category_id': category_id}
+    return render(request, 'delete_category.html', context)
