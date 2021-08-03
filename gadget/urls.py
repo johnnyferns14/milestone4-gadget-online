ac262f349145ab449265278864e8d72c625b37a8
johnnyferns14 <johnnyferns@gmail.com>
2021-08-03 03:33:40 +0000

    Added form for adding category

--- gadget/urls.py
+++ gadget/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_gadget, name='view_gadget'),
+    path('add_category/', views.add_category, name='add_category'),
 ]
--- gadget/views.py
+++ gadget/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import CategoryForm
 
 
 def view_gadget(request):
     context = {}
     return render(request, 'gadget_view.html', context)
+
+
+def add_category(request):
+    form = CategoryForm()
+    context = {'form': form}
+    return render(request, 'add_category.html', context)
