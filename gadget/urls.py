bfd16ae5338d05b8caac3153fd394aca1e329c4b
johnnyferns14 <johnnyferns@gmail.com>
2021-08-07 16:17:48 +0000

    Update and delete functionality added

--- gadget/urls.py
+++ gadget/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_gadget, name='view_gadget'),
+    # path('', views.view_gadget, name='view_gadget'),
+    path('', views.view_category, name='view_category'),
     path('add_category/', views.add_category, name='add_category'),
+    path('update_category/<category_id>', views.update_category, name='update_category'),
+    path('delete_category/<category_id>', views.delete_category, name='delete_category'),
 ]
--- gadget/views.py
+++ gadget/views.py
@@ -1,32 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CategoryForm
 from .models import Category
 
 
-def view_categories(request):
+def view_category(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
-    return render(request, 'categories_view.html', context)
+    context = {
+        'categories': categories
+        }
+    return render(request, 'category_view.html', context)
+
+
+
+def add_category(request): 
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CategoryForm()
+    context = {'form': form}
+    return render(request, 'add_category.html', context)
+
+
+def update_category(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    form = CategoryForm(request.POST or None, instance=category)
+    if form.is_valid():
+        form.save()
+        return redirect('view_category')
+    context = {
+        'category': category,
+        'form': form
+        }
+
+    return render(request, 'update_category.html', context)
+
+
+def delete_category(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    category.delete()
+    return redirect('view_category')
 
 
 def view_gadget(request):
     context = {}
     return render(request, 'gadget_view.html', context)
-
-
-def add_category(request):
-    form = CategoryForm()
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        if form.is_valid:
-            form.save()
-    context = {'form': form}
-    return render(request, 'add_category.html', context)
-
-
-def delete_category(request, category_id):
-    category = Category.objects.get(pk=category_id)
-    if request.method == 'POST':
-        category.delete()
-    context = {'category': category, 'category_id': category_id}
-    return render(request, 'delete_category.html', context)
