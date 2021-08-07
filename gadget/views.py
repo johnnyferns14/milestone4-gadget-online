5741e9adf9fa7264d78a50390125d54b70f927ac
johnnyferns14 <johnnyferns@gmail.com>
2021-08-07 17:47:19 +0000

    Image attribute in product model fixed

--- gadget/views.py
+++ gadget/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import CategoryForm
-from .models import Category
+from .forms import CategoryForm, ProductForm
+from .models import Category, Product
 
 
 def view_category(request):
@@ -11,8 +11,7 @@ def view_category(request):
     return render(request, 'category_view.html', context)
 
 
-
-def add_category(request): 
+def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
         if form.is_valid():
@@ -43,6 +42,39 @@ def delete_category(request, category_id):
     return redirect('view_category')
 
 
-def view_gadget(request):
-    context = {}
-    return render(request, 'gadget_view.html', context)
+def view_product(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+        }
+    return render(request, 'product_view.html', context)
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProductForm()
+    context = {'form': form}
+    return render(request, 'add_product.html', context)
+
+
+def update_product(request, product_id):
+    product = Product.objects.get(asin=product_id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('view_product')
+    context = {
+        'product': product,
+        'form': form
+        }
+    return render(request, 'update_product.html', context)
+
+
+def delete_product(request, product_id):
+    product = Product.objects.get(asin=product_id)
+    product.delete()
+    return redirect('view_product')
