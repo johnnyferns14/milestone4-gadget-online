978ebbfd5ac7e01b21176a62742699705eb3d9af
johnnyferns14 <johnnyferns@gmail.com>
2021-08-10 06:29:00 +0000

    models created in review app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from gadget.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProductReview(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review_title = models.CharField(max_length=255, null=False, blank=False)
+    review_body = models.TextField(null=False, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.review_title
