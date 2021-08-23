c2c85fe7eba1886a21ef33a329214ce626b5c2a9
johnnyferns14 <johnnyferns@gmail.com>
2021-08-23 21:43:31 +0000

    created webhook handler for payment failed

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,23 @@
+import json
+import time
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.conf import settings
+from gadget.models import Product
+from .models import Order, OrderLineItem
+
+
+class StripeWH_Handler:
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
