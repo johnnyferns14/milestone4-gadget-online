c12cbb47d31e34a280295be5bef7eae5297d8445
johnnyferns14 <johnnyferns@gmail.com>
2021-08-23 22:34:12 +0000

    Created UserAccount model

--- useraccount/models.py
+++ useraccount/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from django_countries.fields import CountryField
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='userprofile')
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True, blank=True)
+    default_postcode = models.CharField(max_length=20,
+                                        null=True, blank=True)
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
