b283b119bae179b27981e98a3a49b51f7e5c1cba
johnnyferns14 <johnnyferns@gmail.com>
2021-08-23 22:38:34 +0000

    Created post-save signal for useraccount

--- useraccount/models.py
+++ useraccount/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
@@ -23,3 +25,12 @@ class UserAccount(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_account(sender, instance, created, **kwargs):
+
+    if created:
+        UserAccount.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
