43ed3b7759bff30594ad35160ec7ceb4d91056dc
johnnyferns14 <johnnyferns@gmail.com>
2021-09-01 15:26:14 +0000

    Webhook handler updated with user account data

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from gadget.models import Product
 from .models import Order, OrderLineItem
 
+from useraccount.models import UserAccount
+
 
 class StripeWH_Handler:
 
@@ -32,6 +34,21 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserAccount.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -57,10 +74,23 @@ class StripeWH_Handler:
         if order_exists:
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
-                         'Verified order already in database'),
-                status=200)
+                         'Verified order already in database'), status=200)
         else:
+            order = None
             try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    user_profile=profile,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
                 for item_id, item_data in json.loads(cart).items():
                     order = order.objects.create(
                         full_name=shipping_details.name,
