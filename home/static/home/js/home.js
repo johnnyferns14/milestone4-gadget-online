95521c03333ab4d95d90569d02a56907b500b75f
johnnyferns14 <johnnyferns@gmail.com>
2021-09-03 12:01:19 +0000

    Bootstrap toasts added

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,10 +1,4 @@
-let loginButton = document.getElementById('loginbtn');
-let loginWrapper = document.getElementById('login-wrapper')
-
-loginButton.addEventListener('click', function() {
-  loginWrapper.classList.toggle("login-page-reveal");
-  loginWrapper.classList.toggle("");
-});
-
-
-$('.toast').toast(option)
+$(document).ready(function(){
+    $('.toast').toast('show');
+  });
+ 
