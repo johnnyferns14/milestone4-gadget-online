a78ecf23705db13fa993d5af5d9f30654303f855
johnnyferns14 <johnnyferns@gmail.com>
2021-09-06 08:07:44 +0000

    Deploy to heroku

--- gadget360/settings.py
+++ gadget360/settings.py
@@ -187,11 +187,11 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    else:
-        STATIC_URL = '/static/'
-        STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static'),
-        ]
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
